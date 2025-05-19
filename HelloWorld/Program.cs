81b4253b8449c626dda7b50d4064a8a0c4d60f5a
CalvinGaiennie <calvingaiennie@gmail.com>
2025-05-19 13:09:28 -0500

    Continued Course

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,9 +6,31 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-         Console.WriteLine("Hello, World!");
-         Console.Write("OK");
-         Console.Write("Second");   
+            string[] myGroceryArray = new string[2];
+
+            myGroceryArray[0] = "Cheese";
+            myGroceryArray[1] = "Milk";
+
+            // List<string> myGroceryList = new List<string>();
+            // myGroceryList.Add("Ice Cream");
+
+            List<string> myGroceryList = ["Ice Cream", "Crackers"];
+
+            Console.WriteLine(myGroceryList[0]);
+
+            IEnumerable<string> myGroceryEnumerable = myGroceryList;
+
+            List<string> mySecondGroceryList = myGroceryEnumerable.ToList();
+
+            string[,] myMultiDemensionalArray = {{"a","b"},{"c","d"},{"e","f"}};
+            Console.WriteLine(myMultiDemensionalArray[0,0]);
+            Console.WriteLine(myMultiDemensionalArray[2,1]);
+
+            Dictionary<string, int> groceryPrices = new Dictionary<string, int>();
+
+            groceryPrices["Cheese"] = 5;
+
+            Console.WriteLine(groceryPrices["Cheese"]);
         }
     }
 }
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58b90778359424ab4b04f13bfccf7e9398913ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
