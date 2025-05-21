6fa3cdde11bd084d4aa247531ccda700762cf527
CalvinGaiennie <calvingaiennie@gmail.com>
2025-05-20 20:23:20 -0500

    did another lesson

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -35,36 +35,124 @@ namespace HelloWorld
 
             // Console.WriteLine(myString);
 
-            if (myInt < mySecondInt) 
-            {
-                myInt += 10;
-            }
+            // if (myInt < mySecondInt) 
+            // {
+            //     myInt += 10;
+            // }
 
-            Console.WriteLine(myInt);
+            // Console.WriteLine(myInt);
 
-            string myCow = "Cow";
-            string myCapitalizedCow = "Cow";
+            // string myCow = "Cow";
+            // string myCapitalizedCow = "Cow";
 
-            if (myCow == myCapitalizedCow)
-            {
-                Console.WriteLine("Equal");
-            } else 
-            {
-                Console.WriteLine("Not Equal");
-            }
+            // if (myCow == myCapitalizedCow)
+            // {
+            //     Console.WriteLine("Equal");
+            // } else 
+            // {
+            //     Console.WriteLine("Not Equal");
+            // }
 
-            switch (myCow)
-            {
-                case "cow":
-                    Console.WriteLine("Lowercase");
-                    break;
-                case "Cow":
-                    Console.WriteLine("Uppercase");
-                    break;
-                default:
-                    Console.WriteLine("Default Ran");
-                    break;
-            }
+            // switch (myCow)
+            // {
+            //     case "cow":
+            //         Console.WriteLine("Lowercase");
+            //         break;
+            //     case "Cow":
+            //         Console.WriteLine("Uppercase");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Default Ran");
+            //         break;
+            // }
+
+            // int[] intsToCompress = new int[] {10,15,20,25,30,12,34, 5};
+
+
+            // DateTime startTime = DateTime.Now;
+
+
+            // int totalValue = intsToCompress[0] + intsToCompress[1] + intsToCompress[2] + intsToCompress[3] + intsToCompress[4] + intsToCompress[5] + intsToCompress[6];
+
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            // Console.WriteLine(totalValue);
+
+            // totalValue = 0;
+
+            // startTime = DateTime.Now;
+
+            // for (int i = 0; i < intsToCompress.Length; i++)
+            // {
+            //     totalValue += intsToCompress[i];
+            // }
+            // Console.WriteLine("For");
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            // Console.WriteLine(totalValue);
+
+            // totalValue = 0;
+
+            // startTime = DateTime.Now;
+
+            // foreach(int intForCompression in intsToCompress)
+            // {
+            //     totalValue += intForCompression;
+            // }
+            // Console.WriteLine("For Each");
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            // Console.WriteLine(totalValue);
+
+
+
+            // totalValue = 0;
+
+            // startTime = DateTime.Now;
+
+            // int index = 0;
+
+            // while(index < intsToCompress.Length)
+            // {
+
+            //     totalValue += intsToCompress[index];
+            //     index++;
+
+            // }
+            // Console.WriteLine("while");
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            // Console.WriteLine(totalValue);
+
+            // totalValue = 0;
+
+            // startTime = DateTime.Now;
+
+            // index = 0;
+
+            // do
+            // {
+
+            //     totalValue += intsToCompress[index];
+            //     index++;
+
+            // } while(index < intsToCompress.Length);
+
+
+            // Console.WriteLine("do while");
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            // Console.WriteLine(totalValue);
+
+            // totalValue = 0;
+
+            // totalValue = intsToCompress.Sum();
+
+            //  Console.WriteLine("built in method");
+            // Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            // Console.WriteLine(totalValue);
+
+            
         }
+        
     }
 }
