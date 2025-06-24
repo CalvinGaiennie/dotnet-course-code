f7cf1e2594ed7a11e3d9552f27934e6b4d9e7a3e
CalvinGaiennie <calvingaiennie@gmail.com>
2025-06-23 19:43:14 -0500

    continued course

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,72 +9,100 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-
+using Newtonsoft.Json.Serialization;
+using AutoMapper;
 
 namespace HelloWorld
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-          DataContextDapper dapper = new DataContextDapper(config);
+      IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+      DataContextDapper dapper = new DataContextDapper(config);
 
-          // string formattedDate = myComputer.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss");
+      // string formattedDate = myComputer.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss");
 
-          // string sql = @"INSERT INTO TutorialAppSchema.Computer (
-          //     Motherboard,
-          //     HasWifi,
-          //     HasLTE,
-          //     ReleaseDate,
-          //     Price,
-          //     VideoCard
-          //  ) VALUES ('" + myComputer.Motherboard 
-          //  + "','" + myComputer.HasWifi
-          //  + "','" + myComputer.HasLTE
-          //  + "','" + formattedDate
-          //  + "','" + myComputer.Price
-          //  + "','" + myComputer.HasWifi
-          //  +"')\n";
+      // string sql = @"INSERT INTO TutorialAppSchema.Computer (
+      //     Motherboard,
+      //     HasWifi,
+      //     HasLTE,
+      //     ReleaseDate,
+      //     Price,
+      //     VideoCard
+      //  ) VALUES ('" + myComputer.Motherboard 
+      //  + "','" + myComputer.HasWifi
+      //  + "','" + myComputer.HasLTE
+      //  + "','" + formattedDate
+      //  + "','" + myComputer.Price
+      //  + "','" + myComputer.HasWifi
+      //  +"')\n";
 
-          //  using StreamWriter openFile = new("log.txt", append: true);
+      //  using StreamWriter openFile = new("log.txt", append: true);
 
-          //  openFile.WriteLine(sql);
+      //  openFile.WriteLine(sql);
 
-          //  openFile.Close();
+      //  openFile.Close();
 
-          //  string fileText = File.ReadAllText("log.txt");
+      //  string fileText = File.ReadAllText("log.txt");
 
-          string computersJson = File.ReadAllText("Computers.json");
+      string computersJson = File.ReadAllText("ComputersSnake.json");
 
-          // Console.WriteLine(computersJson);
+      Mapper mapper = new Mapper();
+
+      // Console.WriteLine(computersJson);
 
 
-          JsonSerializerOptions options = new JsonSerializerOptions()
-          {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-          };
+      // JsonSerializerOptions options = new JsonSerializerOptions()
+      // {
+      //   PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+      // };
 
-          // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+      // // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
 
-          IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+      // IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
 
-          if (computers != null)
-          {
-            foreach( Computer computer in computers)
-            {
-              // Console.WriteLine(computer.Motherboard);
-            };
-          }
+      // if (computers != null)
+      // {
+      //   foreach (Computer computer in computers)
+      //   {
+      //     // Console.WriteLine(computer.Motherboard);
+      //     string formattedDate = (computer.ReleaseDate ?? DateTime.Now).ToString("yyyy-MM-dd HH:mm:ss");
+      //     string sql = @"INSERT INTO TutorialAppSchema.Computer (
+      //         Motherboard,
+      //         HasWifi,
+      //         HasLTE,
+      //         ReleaseDate,
+      //         Price,
+      //         VideoCard
+      //      ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+      //       + "','" + computer.HasWifi
+      //       + "','" + computer.HasLTE
+      //       + "','" + formattedDate
+      //       + "','" + computer.Price
+      //       + "','" + EscapeSingleQuote(computer.VideoCard)
+      //       + "')";
 
-          JsonSerializerSettings settings = new JsonSerializerSettings()
-          {
-            ContractResolver = new CamelCasePropertyNamesContracResolver()
-          };
+      //     dapper.ExecuteSql(sql);
 
-          string computersCopyNewtonsoft = JsonConvert.SerializeObject(computers, settings);
+      //   }
+      // }
 
-          File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
-        }
+      // JsonSerializerSettings settings = new JsonSerializerSettings()
+      // {
+      //   ContractResolver = new CamelCasePropertyNamesContractResolver()
+      // };
+
+      // string computersCopyNewtonsoft = JsonConvert.SerializeObject(computers, settings);
+
+      // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+    }
+
+      static string EscapeSingleQuote(string input)
+      {
+        string output = input.Replace("'", "''");
+
+        return output;
+      }
     }
 }
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59469aee62b7378df643e66fcacc644ca03cd20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f70780dd25682411dfd010a350c16b2cb0f88db")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
