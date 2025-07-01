94848e892e4962583709e30ba3d183f00c128ce5
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-01 12:16:38 -0500

    did the next few lessons of the course. Started building a basic API

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -16,132 +16,39 @@ namespace HelloWorld
 {
   internal class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-      IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-      DataContextDapper dapper = new DataContextDapper(config);
+      Task firstTask = new Task(() => {
+        Thread.Sleep(100);
+        Console.WriteLine("Task 1");
+      });
+      firstTask.Start();
 
-      // string formattedDate = myComputer.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss");
+      Task secondTask = ConsoleAfterDelayAsync("Task 2 ", 150);
 
-      // string sql = @"INSERT INTO TutorialAppSchema.Computer (
-      //     Motherboard,
-      //     HasWifi,
-      //     HasLTE,
-      //     ReleaseDate,
-      //     Price,
-      //     VideoCard
-      //  ) VALUES ('" + myComputer.Motherboard 
-      //  + "','" + myComputer.HasWifi
-      //  + "','" + myComputer.HasLTE
-      //  + "','" + formattedDate
-      //  + "','" + myComputer.Price
-      //  + "','" + myComputer.HasWifi
-      //  +"')\n";
+      ConsoleAfterDelay("Delay", 75);
 
-      //  using StreamWriter openFile = new("log.txt", append: true);
+      Task thirdTask = ConsoleAfterDelayAsync("Task 3 ", 50);
 
-      //  openFile.WriteLine(sql);
+      await secondTask;
+      await firstTask;
+      Console.WriteLine("After the Task was created");
+      await thirdTask;
+    }
 
-      //  openFile.Close();
-
-      //  string fileText = File.ReadAllText("log.txt");
-
-      string computersJson = File.ReadAllText("ComputersSnake.json");
-
-      Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
-      {
-        cfg.CreateMap<ComputerSnake, Computer>()
-          .ForMember(destination => destination.ComputerId, options =>
-            options.MapFrom(source => source.computer_id))
-          .ForMember(destination => destination.CPUCores, options =>
-            options.MapFrom(source => source.cpu_cores))
-          .ForMember(destination => destination.HasLTE, options =>
-            options.MapFrom(source => source.has_lte))
-          .ForMember(destination => destination.HasWifi, options =>
-            options.MapFrom(source => source.has_wifi))
-          .ForMember(destination => destination.Motherboard, options =>
-            options.MapFrom(source => source.motherboard))
-          .ForMember(destination => destination.Price, options =>
-            options.MapFrom(source => source.price))
-          .ForMember(destination => destination.ReleaseDate, options =>
-            options.MapFrom(source => source.release_date))
-          .ForMember(destination => destination.VideoCard, options =>
-            options.MapFrom(source => source.video_card));
-      }));
-
-      // Console.WriteLine(computersJson);
-
-
-      // JsonSerializerOptions options = new JsonSerializerOptions()
-      // {
-      //   PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-      // };
-
-      IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson);
-
-    if (computersSystem != null)
+    static void ConsoleAfterDelay(string text, int delayTime)
     {
-      IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
-      Console.WriteLine(computerResult.Count());
-      // foreach (Computer computer in computerResult)
-      // {
-      //   Console.WriteLine(computer.Motherboard);
-      // }
+      Thread.Sleep(delayTime);
+      Console.WriteLine(text);
     }
-    IEnumerable<Computer>? computersSystemJsonPropertyMapping = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson);
-
-     if (computersSystemJsonPropertyMapping != null)
+    static async Task ConsoleAfterDelayAsync(string text, int delayTime)
     {
-      Console.WriteLine(computersSystemJsonPropertyMapping.Count());
-      // foreach (Computer computer in computersSystemJsonPropertyMapping)
-      // {
-      //   Console.WriteLine(computer.Motherboard);
-      // }
+      await Task.Delay(delayTime);
+      Console.WriteLine(text);
     }
 
-      // IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
 
-      // if (computers != null)
-      // {
-      //   foreach (Computer computer in computers)
-      //   {
-      //     // Console.WriteLine(computer.Motherboard);
-      //     string formattedDate = (computer.ReleaseDate ?? DateTime.Now).ToString("yyyy-MM-dd HH:mm:ss");
-      //     string sql = @"INSERT INTO TutorialAppSchema.Computer (
-      //         Motherboard,
-      //         HasWifi,
-      //         HasLTE,
-      //         ReleaseDate,
-      //         Price,
-      //         VideoCard
-      //      ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
-      //       + "','" + computer.HasWifi
-      //       + "','" + computer.HasLTE
-      //       + "','" + formattedDate
-      //       + "','" + computer.Price
-      //       + "','" + EscapeSingleQuote(computer.VideoCard)
-      //       + "')";
-
-      //     dapper.ExecuteSql(sql);
-
-      //   }
-      // }
-
-      // JsonSerializerSettings settings = new JsonSerializerSettings()
-      // {
-      //   ContractResolver = new CamelCasePropertyNamesContractResolver()
-      // };
-
-      // string computersCopyNewtonsoft = JsonConvert.SerializeObject(computers, settings);
-
-      // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
-    }
-
-      static string EscapeSingleQuote(string input)
-      {
-        string output = input.Replace("'", "''");
-
-        return output;
-      }
-    }
+  }
 }
+
+
