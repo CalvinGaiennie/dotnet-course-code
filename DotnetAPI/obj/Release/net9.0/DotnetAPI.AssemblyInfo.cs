5cb81fe068ab7006a25f56d0de970987675f0ad0
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-14 14:55:38 -0500

    ran a build to host

--- DotnetAPI/obj/Release/net9.0/DotnetAPI.AssemblyInfo.cs
+++ DotnetAPI/obj/Release/net9.0/DotnetAPI.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf9cd3f250251158b90e8586018c66d7e17c00e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
